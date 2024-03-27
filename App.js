1532b5c0352ecc06e2f0d32e70f6613052f60d98
Edward Chiang <edwardc@hydratedcompany.com>
2024-03-27 23:54:21 +0800

    Add navigation bar component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import MyMap from './mymap_component.js'
+import MainNavigationBar from './navigation_bar.js'
 
 export default function App() {
 
@@ -14,6 +15,7 @@ export default function App() {
                     <MyMap />
                 </View>
                 <View style={{ height: "8%", backgroundColor: "#DDDDDD", flexDirection: "column" }}>
+                    <MainNavigationBar />
                 </View>
             </SafeAreaView>
         </NavigationContainer>
---
+++ navigation_bar.js
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import * as React from 'react';
+import { Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { supabase } from './libs/supabase.js'
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}></View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}></View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function MainNavigationBar() {
+    const [loading, setLoading] = useState(true)
+    const [navigationName, setNavigationName] = useState('')
+
+    async function getNavigationName() {
+        try {
+            setLoading(true)
+
+            const { data, error, status } = await supabase
+                .from('test_table')
+                .select()
+                .order('name', { ascending: true })
+                .limit(1)
+                .single()
+
+            console.log('data:', data)
+            console.log('error:', error)
+            console.log('status:', status)
+
+            if (error) {
+                throw error
+            }
+
+            if (data) {
+                setNavigationName(data.name)
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                Alert.alert(error.message)
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarLabelPosition: 'below-icon',
+                    tabBarIcon: () => (
+                        <Image source={require('./assets/home.png')} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Advertisement"
+                component={SettingsScreen}
+                options={{
+                    tabBarLabel: 'Advertisement',
+                    tabBarLabelPosition: 'below-icon',
+                    tabBarIcon: () => (
+                        <Image source={require('./assets/presention-chart.png')} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Subscription"
+                component={SettingsScreen}
+                options={{
+                    tabBarLabel: 'Subscription',
+                    tabBarLabelPosition: 'below-icon',
+                    tabBarIcon: () => (
+                        <Image source={require('./assets/dollar-square.png')} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={SettingsScreen}
+                options={{
+                    // tabBarLabel: 'Profile',
+                    tabBarLabel: () => (
+                        <Text>{loading ? 'Loading ...' : navigationName}</Text>
+                    ),
+                    tabBarLabelPosition: 'below-icon',
+                    tabBarIcon: () => (
+                        <Image source={require('./assets/profile-circle.png')} />
+                    ),
+                    tabBarButton: props => (
+                        <TouchableOpacity {...props} onPress={() => getNavigationName()} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
